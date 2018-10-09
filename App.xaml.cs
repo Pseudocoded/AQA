391ac3999115f3d0b025c74bf4bb05a3e397f735
shikii23 <limchingjen@gmail.com>
2018-10-09 15:06:27 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using AirQA.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace AirQA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+           // MainPage = new MainPage();
+
+            MainPage = new MasterDetailPage()
+            {
+                Master = new MasterPage(),
+                Detail = new NavigationPage(new MasterPage())
+            };
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
